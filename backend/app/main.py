763229af1cdb646e821bd0218021d13e8608f36e
anggars <anggariantosudrajat@gmail.com>
2025-08-01 18:53:38 +0700

    whatever

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,11 +8,9 @@ from app.db_handler import init_db
 load_dotenv()
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(router)
 
 @app.on_event("startup")
 def on_startup():
     init_db()
-
-# Serve frontend from public/
-app.mount("/", StaticFiles(directory="public", html=True), name="frontend")
