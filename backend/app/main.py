3a3458578a6c4230a1a48a4016e4a3dd09332ca0
anggars <anggariantosudrajat@gmail.com>
2025-08-01 22:08:59 +0700

    adapt to zeabur

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,12 @@
+# backend/app/main.py
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from dotenv import load_dotenv
 
 from app.routes import router
-from app.db_handler import init_db
+# Ganti import ini
+from app.db_handler import init_db_pool, create_tables
 
 load_dotenv()
 
@@ -13,4 +16,7 @@ app.include_router(router)
 
 @app.on_event("startup")
 def on_startup():
-    init_db()
+    # Ganti panggilan fungsi ini
+    init_db_pool()
+    # Kita juga bisa panggil create_tables di sini agar tabel selalu ada
+    create_tables()
