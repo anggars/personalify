870cebddcd9f40df3b647c459eb996d8546e6a70
anggars <anggariantosudrajat@gmail.com>
2026-02-12 19:09:27 +0700

    update build gradle

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.aritsu.personalify"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
